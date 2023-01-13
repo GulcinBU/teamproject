63fa9dd94857f368676d452df1b1728e257c824c
Gulcin Uluyazi <86480728+GulcinBU@users.noreply.github.com>
2023-01-13 15:09:37 +0100

    Add files via upload

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ from streamlit_extras.app_logo import add_logo
 
 
 
-st.set_page_config(page_title="DataLimn", page_icon="👋")
+st.set_page_config(page_title="Hello", page_icon="👋")
 
 #logo
 def add_logo(logo_url: str):
@@ -42,33 +42,27 @@ add_logo("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgHYxaZGEwO_
 
 
 
-st.write("# Welcome to DataLimn! 👋")
+st.write("# Welcome to DataLimn!")
 
 
 
 st.markdown(
     """
-    
-DataLimn is your go-to open-source framework for machine learning and data science projects. The easy to use app gives greater insight and detailed information about your data. You simply upload a CSV file, follow some steps, and get your results!
-
+    DataLimn is your go-to open-source framework for machine learning and data science projects. The easy to use app gives greater insight and detailed information about your data. You simply upload a CSV file, follow some steps, and get your results!
 ### What does DataLimn do?
 DataLimn is a tool designed to analyse patterns and make predictions without having to have previous knowledge of coding. This no-code machine learning app evaluates and interprets your data, transforming it into an understandable story that can be of great value in business decision-making processes. 
-
-
 # Module description
-
 ### Data uploading and preparation
 Upload a CSV file with your data. Once the data is uploaded the user can change the data type per column and select the training size. Furthermore , select the columns you would want to take into consideration as well as the column where you want to base your predictions on.
-
 ### Machine learning and data visualisation
 The next step is to select one (or multiple) learning model(s) based on your data and the outcome you would like to see. Now it is time for our app to do the hard work. 
-
-The app will give you predictions and probabilities based on your data input, and some statistical graphs can be selected. Moreover, you can modify your input and generate a new outcome based on the features given. 
+The app will give you predictions and probabilities based  on your data input, and some statistical graphs can be selected. Moreover, you can modify your input and generate a new outcome based on the features given. 
 
 """
 )
 
-
-next = st.button("Next")
+st.write(' ')
+st.write(' ')
+next = st.button("next")
 if next:
     switch_page("userlogin")
